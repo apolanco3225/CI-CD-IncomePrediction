1953a32d6a1ed116a0ddc920a7f3b022877e855b
Arturo Polanco Lozano <apolanco3225@gmail.com>
2022-02-05 10:45:30 -0500

    update eda

---
+++ app/sample_file.py
@@ -0,0 +1,15 @@
+import requests
+import json 
+
+data = {
+    "name": "Hitchiking Kit",
+    "tags": ["book", "towel"],
+    "item_id": 23
+}
+
+r = requests.post(
+    "http://127.0.0.1:8000/items/", 
+    data=json.dumps(data)
+)
+
+print(r.json())
