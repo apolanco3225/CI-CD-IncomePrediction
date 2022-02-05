5ade86844f3090cd1456923213a9a930277363d2
Arturo Polanco Lozano <apolanco3225@gmail.com>
2022-02-05 11:05:18 -0500

    update in the api

--- app/local_test.py
+++ app/local_test.py
@@ -1,22 +1,54 @@
-from fastapi.testclient import TestClient
-
-from foo import app
-
-client = TestClient(app)
-
-
-def test_get_path():
-    r = client.get("/items/42")
+def test_get(client):
+    r = client.get("/")
     assert r.status_code == 200
-    assert r.json() == {"fetch": "Fetched 1 of 42"}
+    assert r.json() == {"message": "Greetings!"}
 
 
-def test_get_path_query():
-    r = client.get("/items/42?count=5")
-    assert r.status_code == 200
-    assert r.json() == {"fetch": "Fetched 5 of 42"}
+def test_post_high(client):
+    request = client.post("/", json={'age': 33,
+                                     'workclass': 'Private',
+                                     'fnlgt': 149184,
+                                     'education': 'HS-grad',
+                                     'marital_status': 'Never-married',
+                                     'occupation': 'Prof-specialty',
+                                     'relationship': 'Not-in-family',
+                                     'race': 'White',
+                                     'sex': 'Male',
+                                     'hoursPerWeek': 60,
+                                     'nativeCountry': 'United-States'
+                                     })
+    assert request.status_code == 200
+    assert request.json() == {"prediction": ">50K"}
 
 
-def test_get_malformed():
-    r = client.get("/items")
-    assert r.status_code != 200
+def test_post_low(client):
+    request = client.post("/", json={'age': 19,
+                                     'workclass': 'Private',
+                                     'fnlgt': 149184,
+                                     'education': 'HS-grad',
+                                     'marital_status': 'Never-married',
+                                     'occupation': 'Prof-specialty',
+                                     'relationship': 'Not-in-family',
+                                     'race': 'White',
+                                     'sex': 'Male',
+                                     'hoursPerWeek': 60,
+                                     'nativeCountry': 'United-States'
+                                     })
+    assert request.status_code == 200
+    assert request.json() == {"prediction": "<=50K"}
+
+
+def test_post_malformed(client):
+    r = client.post("/", json={
+        "age": 32,
+        "workclass": "",
+        "education": "Some-college",
+        "maritalStatus": "",
+        "occupation": "",
+        "relationship": "Husband",
+        "race": "Black",
+        "sex": "Male",
+        "hoursPerWeek": 60,
+        "nativeCountry": "United-States"
+    })
+    assert r.status_code == 422
