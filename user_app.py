0292f51fd03172ec12fc3ada02d12a335dfc960c
Arturo Polanco Lozano <apolanco3225@gmail.com>
2022-02-14 19:43:58 -0500

    fixing deployment issues

--- user_app.py
+++ user_app.py
@@ -4,13 +4,20 @@ Author: Arturo Polanco
 Date: February 2022
 """
 # import necessary packages
-
+import os
+import sys
 from fastapi import FastAPI
 from schema import ModelInput
 from inference_pipeline import inference
 
 
 
+# setting up config
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system("dvc pull") != 0:
+        sys.exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
 
 # instantiating fastapi
 app = FastAPI()
